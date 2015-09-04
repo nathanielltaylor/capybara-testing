4ecb13c7d08a31f53c7355dfbb34bd2dcc27b4a8
nathanielltaylor <ntaylor390@gmail.com>
2015-09-04 17:01:03 -0400

    initial

---
+++ spec/user_checks_a_recipe_spec.rb
@@ -0,0 +1,67 @@
+require "spec_helper"
+
+feature "User checks a recipe's deliciousness", %(
+  As a user
+  I want to submit a recipe name to see if it is delicious
+  So that I know with confidence what to cook.
+
+  Acceptance Criteria:
+  [X] When I visit the root path, I can see a form to submit a recipe name
+  [X] If I submit a recipe name with "pickled beets" in the name, I am
+      sent to a "results" page telling me that the recipe is delicious
+  [X] If I submit a recipe name without "pickled beets" in the name, I am
+      sent to a "results" page telling me that the recipe is not delicious
+  [X] From the "results" page, I am able to click a link bringing me back to
+      the home page
+  [X] If I submit a blank entry to the form, I am brought to an error page
+  [X] From the error page, I can click a link bringing me back to the home page
+
+) do
+
+  scenario "user sees submit form on home page" do
+    visit "/"
+    expect(page).to have_selector("input[type=submit][value='Submit']")
+  end
+
+  scenario "user submits a recipe name containing 'pickled beets'" do
+    visit "/"
+    fill_in 'recipe_name', with: 'pickled beets'
+    click_button 'Submit'
+    expect(page).to have_content("is a delicious recipe!")
+  end
+
+  scenario "user submits a recipe name without 'pickled beets'" do
+    visit "/"
+    fill_in 'recipe_name', with: 'cheese fondue'
+    click_button 'Submit'
+    expect(page).to have_content("is not a delicious recipe!")
+  end
+
+  scenario "user navigates back to the home page after checking a recipe name" do
+    visit "/"
+    fill_in 'recipe_name', with: 'cheese fondue'
+    click_button 'Submit'
+    expect(page).to have_content("Try again!")
+    click_link("Try again!")
+    expect(page).to have_content('Instructions: Enter a recipe name here, and click "Submit" to see
+    if the recipe will be delicious or not!')
+  end
+
+  scenario "user submits an empty form" do
+    visit "/"
+    fill_in 'recipe_name', with: ''
+    click_button 'Submit'
+    expect(page).to have_content("You can't submit an empty recipe name!")
+  end
+
+  scenario "user navigates back to the home page after submitting an empty form" do
+    visit "/"
+    fill_in 'recipe_name', with: ''
+    click_button 'Submit'
+    expect(page).to have_content("You can't submit an empty recipe name!")
+    expect(page).to have_content("Try again!")
+    click_link("Try again!")
+    expect(page).to have_content('Instructions: Enter a recipe name here, and click "Submit" to see
+    if the recipe will be delicious or not!')
+  end
+end
